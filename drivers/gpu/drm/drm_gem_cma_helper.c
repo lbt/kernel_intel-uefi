93ee1d7916e767720d5cadb7c5db6f040ae17629
Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
2013-02-17 01:54:26 +0100

    drm: GEM CMA: Split object creation into object alloc and DMA memory alloc
    This allows creating a GEM CMA object without an associated DMA memory
    buffer, and will be used to implement DRM PRIME support.
    
    Change-Id: I957122119d1db24725a0616ab77ef1f175696928
    Signed-off-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    Reviewed-by: Rob Clark <robdclark@gmail.com>

--- drivers/gpu/drm/drm_gem_cma_helper.c
+++ drivers/gpu/drm/drm_gem_cma_helper.c
@@ -47,7 +47,7 @@ static struct drm_gem_cma_object *
 __drm_gem_cma_create(struct drm_device *drm, unsigned int size)
 {
 	struct drm_gem_cma_object *cma_obj;
-	struct drm_gem_object *gem_obj;
+	struct sg_table *sgt = NULL;
 	int ret;
 
 	cma_obj = kzalloc(sizeof(*cma_obj), GFP_KERNEL);
@@ -83,8 +83,6 @@ struct drm_gem_cma_object *drm_gem_cma_create(struct drm_device *drm,
 		unsigned int size)
 {
 	struct drm_gem_cma_object *cma_obj;
-	struct sg_table *sgt = NULL;
-	int ret;
 
 	size = round_up(size, PAGE_SIZE);
 
@@ -97,29 +95,11 @@ struct drm_gem_cma_object *drm_gem_cma_create(struct drm_device *drm,
 	if (!cma_obj->vaddr) {
 		dev_err(drm->dev, "failed to allocate buffer with size %d\n",
 			size);
-		ret = -ENOMEM;
-		goto error;
+		drm_gem_cma_free_object(&cma_obj->base);
+		return ERR_PTR(-ENOMEM);
 	}
-
-	sgt = kzalloc(sizeof(*cma_obj->sgt), GFP_KERNEL);
-	if (sgt == NULL) {
-		ret = -ENOMEM;
-		goto error;
-	}
-
-	ret = dma_get_sgtable(drm->dev, sgt, cma_obj->vaddr,
-			      cma_obj->paddr, size);
-	if (ret < 0)
-		goto error;
-
-	cma_obj->sgt = sgt;
 
 	return cma_obj;
-
-error:
-	kfree(sgt);
-	drm_gem_cma_free_object(&cma_obj->base);
-	return ERR_PTR(ret);
 }
 EXPORT_SYMBOL_GPL(drm_gem_cma_create);
 
@@ -177,16 +157,9 @@ void drm_gem_cma_free_object(struct drm_gem_object *gem_obj)
 
 	cma_obj = to_drm_gem_cma_obj(gem_obj);
 
-	if (cma_obj->vaddr) {
+	if (cma_obj->vaddr)
 		dma_free_writecombine(gem_obj->dev->dev, cma_obj->base.size,
 				      cma_obj->vaddr, cma_obj->paddr);
-		if (cma_obj->sgt) {
-			sg_free_table(cma_obj->sgt);
-			kfree(cma_obj->sgt);
-		}
-	} else if (gem_obj->import_attach) {
-		drm_prime_gem_destroy(gem_obj, cma_obj->sgt);
-	}
 
 	drm_gem_object_release(gem_obj);
 
