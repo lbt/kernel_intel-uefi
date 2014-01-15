Asus-T100/kernel

drm/i915: handle CACHE_MODE_0 register address change for IVB+ platforms
BZ: 111055

Based on the BSpec. for IVB+ platforms, CACHE_MODE_0 addr has been changed to 0x7000.
Also enabled back the restore of this register in resume path.
 - Restore for this register was disabled for VLV in 107990. This was to avoid the hang
seen because of Address conflict with VLV_PCBR register.

Change-Id: I329773af0c046316de79f5d1d9a2e9c9f13efde4
Signed-off-by: naresh kumar kachhi <naresh.kumar.kachhi@intel.com>

drivers/gpu/drm/i915/i915_reg.h
@@ -698,7 +698,12 @@
 #define   MI_ARB_DISPLAY_PRIORITY_A_B		(0 << 0)	/* display A > display B */
 #define   MI_ARB_DISPLAY_PRIORITY_B_A		(1 << 0)	/* display B > display A */
 
-#define CACHE_MODE_0	0x02120 /* 915+ only */
+#define CACHE_MODE_0	0x02120 /* 915+ only Pre-IVB */
+#define GEN7_CACHE_MODE_0	0x07000 /* IVB+ */
+/* CACHE_MODE_0 offset is different for per-IVB and IVB+ systems */
+#define CACHE_MODE_0_OFFSET(d) ((INTEL_INFO(d)->gen >= 7) ? \
+					GEN7_CACHE_MODE_0 : CACHE_MODE_0)
+
 #define   CM0_IZ_OPT_DISABLE      (1<<6)
 #define   CM0_ZR_OPT_DISABLE      (1<<5)
 #define	  CM0_STC_EVICT_DISABLE_LRA_SNB	(1<<5)

lbt/kernel_intel-uefi

Ported from 3.4 patch: http://android.intel.com:8080/#/c/111053/8
Change-Id: I30584e04ae43b65bf2dd1ac26ca5deb430803adc

drivers/gpu/drm/i915/i915_reg.h
@@ -907,6 +907,9 @@
 #define   ECO_FLIP_DONE		(1<<0)
 
 #define CACHE_MODE_1		0x7004 /* IVB+ */
+/* CACHE_MODE_0 offset is different for per-IVB and IVB+ systems */
+#define CACHE_MODE_0_OFFSET(d) ((INTEL_INFO(d)->gen >= 7) ? \
+					CACHE_MODE_1 : CACHE_MODE_0)
 #define   PIXEL_SUBSPAN_COLLECT_OPT_DISABLE (1<<6)
 
 #define GEN6_BLITTER_ECOSKPD	0x221d0