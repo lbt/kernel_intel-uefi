kernelorg-mirror/linux_kernel_git_mel_linux-balancenuma

drm/i915: Be more informative when reporting "too large for aperture" error
This should help debugging the truly unexpected cases where it occurs -
in particular to see which value is garbage.

References: https://bugzilla.kernel.org/show_bug.cgi?id=58511
Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

drivers/gpu/drm/i915/i915_gem.c
@@ -2975,7 +2975,10 @@ i915_gem_object_bind_to_gtt(struct drm_i915_gem_object *obj,
 	 */
 	if (obj->base.size >
 	    (map_and_fenceable ? dev_priv->gtt.mappable_end : dev_priv->gtt.total)) {
-		DRM_ERROR("Attempting to bind an object larger than the aperture\n");
+		DRM_ERROR("Attempting to bind an object larger than the aperture: object=%ld > %s aperture=%ld\n",
+			  obj->base.size,
+			  map_and_fenceable ? "mappable" : "total",
+			  map_and_fenceable ? dev_priv->gtt.mappable_end : dev_priv->gtt.total);
 		return -E2BIG;
 	}
 

lbt/kernel_intel-uefi

[danvet: s/%ld/%zd/ as spotted by Wu Fengguang's autobuilder.]
(cherry picked from commit a36689cb771f06819c3fa8139c3d3716dfdf6d53)
Change-Id: I08711a06600993dad7781c355a146ec22a617281

drivers/gpu/drm/i915/i915_gem.c
@@ -2970,7 +2970,10 @@ i915_gem_object_bind_to_gtt(struct drm_i915_gem_object *obj,
 	 */
 	if (obj->base.size >
 	    (map_and_fenceable ? dev_priv->gtt.mappable_end : dev_priv->gtt.total)) {
-		DRM_ERROR("Attempting to bind an object larger than the aperture\n");
+		DRM_ERROR("Attempting to bind an object larger than the aperture: object=%zd > %s aperture=%ld\n",
+			  obj->base.size,
+			  map_and_fenceable ? "mappable" : "total",
+			  map_and_fenceable ? dev_priv->gtt.mappable_end : dev_priv->gtt.total);
 		return -E2BIG;
 	}
 