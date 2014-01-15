Asus-T100/kernel

drm/i915: Move simulate hpd to late resume
BZ:130573

Simulate HPD needs to be done after the device is
out of D0ix to ensure we dont encounter deadlocks

Change-Id: I73738dfdb81b845debaada86959d487869f647bc
Signed-off-by: Vidya Srinivas <vidya.srinivas@intel.com>
Reviewed-on: http://android.intel.com:8080/126376
Reviewed-by: Kumar, Shobhit <shobhit.kumar@intel.com>
Reviewed-by: Sharma, Shashank <shashank.sharma@intel.com>
Reviewed-by: Yang, Fei <fei.yang@intel.com>
Reviewed-by: Kachhi, Naresh Kumar <naresh.kumar.kachhi@intel.com>
Tested-by: Sun, Hang L <hang.l.sun@intel.com>
Reviewed-by: cactus <cactus@intel.com>
Tested-by: cactus <cactus@intel.com>

drivers/gpu/drm/i915/intel_display.c
@@ -7514,6 +7514,33 @@ static void intel_pch_pll_init(struct drm_device *dev)
 	}
 }
 
+/*
+Simulate like a hpd event at sleep/resume
+hpd_on =0 >  while suspend, this will clear the modes
+hpd_on =1 >  only at resume  */
+void i915_simulate_hpd(struct drm_device *dev, int hpd_on)
+{
+	struct drm_connector *connector = NULL;
+
+	list_for_each_entry(connector, &dev->mode_config.connector_list, head) {
+		if (connector->polled == DRM_CONNECTOR_POLL_HPD) {
+			if (hpd_on) {
+				/* Resuming, detect and read modes again */
+				connector->funcs->fill_modes(connector,
+				dev->mode_config.max_width,
+				dev->mode_config.max_height);
+			} else {
+				/* Suspend, reset previous detects and modes */
+				if (connector->funcs->reset)
+					connector->funcs->reset(connector);
+			}
+			DRM_DEBUG_KMS("Simulated HPD %s for connector %s\n",
+			(hpd_on ? "On" : "Off"),
+			drm_get_connector_name(connector));
+		}
+	}
+}
+
 static int display_disable_wq(struct drm_device *drm_dev)
 {
 	struct drm_i915_private *dev_priv = drm_dev->dev_private;
@@ -7565,6 +7592,9 @@ ssize_t display_runtime_suspend(struct drm_device *drm_dev)
 	struct intel_encoder *intel_encoder;
 	int audiosts = 0;
 
+	/* Force a re-detection on Hot-pluggable displays */
+	i915_simulate_hpd(drm_dev, false);
+
 	audiosts = mid_hdmi_audio_suspend(drm_dev);
 	if (audiosts != true)
 		DRM_DEBUG_DRIVER("Audio active, CRTC will not be suspended\n");
@@ -7598,6 +7628,10 @@ ssize_t display_runtime_resume(struct drm_device *drm_dev)
 	struct drm_i915_private *dev_priv = drm_dev->dev_private;
 
 	i915_rpm_get_disp(drm_dev);
+
+	/* Re-detect hot pluggable displays */
+	i915_simulate_hpd(drm_dev, true);
+
 	mutex_lock(&drm_dev->mode_config.mutex);
 	dev_priv->disp_pm_in_progress = true;
 

lbt/kernel_intel-uefi

Change-Id: I5b144adcd7b1524c9d55d4962cbde175e9365679

drivers/gpu/drm/i915/intel_display.c
@@ -10048,6 +10048,33 @@ static void intel_shared_dpll_init(struct drm_device *dev)
 		      dev_priv->num_shared_dpll);
 }
 
+/*
+Simulate like a hpd event at sleep/resume
+hpd_on =0 >  while suspend, this will clear the modes
+hpd_on =1 >  only at resume  */
+void i915_simulate_hpd(struct drm_device *dev, int hpd_on)
+{
+	struct drm_connector *connector = NULL;
+
+	list_for_each_entry(connector, &dev->mode_config.connector_list, head) {
+		if (connector->polled == DRM_CONNECTOR_POLL_HPD) {
+			if (hpd_on) {
+				/* Resuming, detect and read modes again */
+				connector->funcs->fill_modes(connector,
+				dev->mode_config.max_width,
+				dev->mode_config.max_height);
+			} else {
+				/* Suspend, reset previous detects and modes */
+				if (connector->funcs->reset)
+					connector->funcs->reset(connector);
+			}
+			DRM_DEBUG_KMS("Simulated HPD %s for connector %s\n",
+			(hpd_on ? "On" : "Off"),
+			drm_get_connector_name(connector));
+		}
+	}
+}
+
 extern void intel_cancel_fbc_work(struct drm_i915_private *dev_priv);
 static int display_disable_wq(struct drm_device *drm_dev)
 {
@@ -10106,6 +10133,9 @@ ssize_t display_runtime_suspend(struct drm_device *dev)
 	struct drm_crtc *crtc;
 	struct intel_encoder *intel_encoder;
 
+	/* Force a re-detection on Hot-pluggable displays */
+	i915_simulate_hpd(dev, false);
+
 	/* ignore lid events during suspend */
 	mutex_lock(&dev_priv->modeset_restore_lock);
 	dev_priv->modeset_restore = MODESET_SUSPENDED;
@@ -10145,6 +10175,9 @@ ssize_t display_runtime_resume(struct drm_device *dev)
 	struct drm_i915_private *dev_priv = dev->dev_private;
 
 	i915_rpm_get_disp(dev);
+
+	/* Re-detect hot pluggable displays */
+	i915_simulate_hpd(dev, true);
 
 	drm_kms_helper_poll_enable(dev);
 	display_save_restore_hotplug(dev, RESTOREHPD);