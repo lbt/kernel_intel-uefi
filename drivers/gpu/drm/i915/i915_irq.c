fae3d6c4f35e989e4667a2f5a629b8bddfa57141
Jon Bloomfield <jon.bloomfield@intel.com>
2014-02-05 10:41:17 +0000

    Revert "REVERTME: dri/915: Integrated VXD Driver to i915 on 3.10."
    This reverts commit 9350a7129282b91e8cbf25fcf737fb283a3fb34b.
    
    The VXD does not compile against the forklifted drm3.13 so is
    being disabled for the initial forklift merge.
    
    Signed-off-by: Jon Bloomfield <jon.bloomfield@intel.com>

--- drivers/gpu/drm/i915/i915_irq.c
+++ drivers/gpu/drm/i915/i915_irq.c
@@ -1326,13 +1326,6 @@ static irqreturn_t valleyview_irq_handler(int irq, void *arg)
 			I915_READ(PORT_HOTPLUG_STAT);
 		}
 
-#ifdef CONFIG_DRM_VXD_BYT
-		if (iir & VED_BLOCK_INTERRUPT) {
-			if (dev_priv->psb_msvdx_interrupt)
-				dev_priv->psb_msvdx_interrupt(dev);
-		}
-#endif
-
 		if (pipe_stats[0] & PIPE_GMBUS_INTERRUPT_STATUS)
 			gmbus_irq_handler(dev);
 
@@ -2501,9 +2494,7 @@ static int valleyview_irq_postinstall(struct drm_device *dev)
 		I915_DISPLAY_PIPE_A_VBLANK_INTERRUPT |
 		I915_DISPLAY_PIPE_B_EVENT_INTERRUPT |
 		I915_DISPLAY_PIPE_B_VBLANK_INTERRUPT;
-#ifdef CONFIG_DRM_VXD_BYT
-	enable_mask |= VED_BLOCK_INTERRUPT;
-#endif
+
 	/*
 	 *Leave vblank interrupts masked initially.  enable/disable will
 	 * toggle them based on usage.
