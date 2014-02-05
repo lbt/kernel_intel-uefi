kitakar5525/linux-kernel

FOR_UPSTREAM [VPG]: drm/i915: Program Valleyview DP M/N registers in case eDP as well
With this change BYT DP M/N registers are programmed. This was needed
to stabilze dual display feature. Without this change HDMI doesn't get
enabled if connected during boot with EFI. Sporadic behaviour with HDMI
hotplug was observed before which is no more reproducible with this
patch.

Issue: APDEV-1039
OTC-Tracker: VIZ-3441
Signed-off-by: Gajanan Bhat <gajanan.bhat@intel.com>
Change-Id: Ibe168f3bfa13f1673c1d6e1a3fef8b5fd1035e86
Reviewed-on: https://vpg-git.iind.intel.com/440
Reviewed-by: Kumar, Shobhit <shobhit.kumar@intel.com>
Tested-by: Kumar, Shobhit <shobhit.kumar@intel.com>
Signed-off-by: Gajanan Bhat <gajanan.bhat@intel.com>
Signed-off-by: A.Sunil Kamath <sunil.kamath@intel.com>

drivers/gpu/drm/i915/intel_display.c
@@ -4539,7 +4539,8 @@ static void ironlake_crtc_enable(struct drm_crtc *crtc)
 	if (intel_crtc->config.has_pch_encoder)
 		intel_prepare_shared_dpll(intel_crtc);
 
-	if (intel_crtc->config.has_dp_encoder)
+	if (intel_crtc->config.has_dp_encoder ||
+		intel_pipe_has_type(&intel_crtc->base, INTEL_OUTPUT_EDP))
 		intel_dp_set_m_n(intel_crtc);
 
 	intel_set_pipe_timings(intel_crtc);
@@ -4651,7 +4652,8 @@ static void haswell_crtc_enable(struct drm_crtc *crtc)
 	if (intel_crtc->active)
 		return;
 
-	if (intel_crtc->config.has_dp_encoder)
+	if (intel_crtc->config.has_dp_encoder ||
+		intel_pipe_has_type(&intel_crtc->base, INTEL_OUTPUT_EDP))
 		intel_dp_set_m_n(intel_crtc);
 
 	intel_set_pipe_timings(intel_crtc);
@@ -5181,7 +5183,8 @@ static void valleyview_crtc_enable(struct drm_crtc *crtc)
 	/* Set up the display plane register */
 	dspcntr = DISPPLANE_GAMMA_ENABLE;
 
-	if (intel_crtc->config.has_dp_encoder)
+	if (intel_crtc->config.has_dp_encoder ||
+		intel_pipe_has_type(&intel_crtc->base, INTEL_OUTPUT_EDP))
 		intel_dp_set_m_n(intel_crtc);
 
 	intel_set_pipe_timings(intel_crtc);
@@ -5286,7 +5289,8 @@ static void i9xx_crtc_enable(struct drm_crtc *crtc)
 	else
 		dspcntr |= DISPPLANE_SEL_PIPE_B;
 
-	if (intel_crtc->config.has_dp_encoder)
+	if (intel_crtc->config.has_dp_encoder ||
+		intel_pipe_has_type(&intel_crtc->base, INTEL_OUTPUT_EDP))
 		intel_dp_set_m_n(intel_crtc);
 
 	intel_set_pipe_timings(intel_crtc);

lbt/kernel_intel-uefi

drivers/gpu/drm/i915/intel_display.c
@@ -5250,7 +5250,8 @@ static void vlv_update_pll(struct intel_crtc *crtc)
 		<< DPLL_MD_UDI_MULTIPLIER_SHIFT;
 	crtc->config.dpll_hw_state.dpll_md = dpll_md;
 
-	if (crtc->config.has_dp_encoder)
+	if (crtc->config.has_dp_encoder ||
+		intel_pipe_has_type(&crtc->base, INTEL_OUTPUT_EDP))
 		intel_dp_set_m_n(crtc);
 
 	mutex_unlock(&dev_priv->dpio_lock);